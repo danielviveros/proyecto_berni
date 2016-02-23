52490492a64202ad1c50df0ff088ace73ac8196c
danielviveros <superman.-1993@hotmail.com>
2016-02-23 11:09:19 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>spa</title>
+</head>
+<body>
+	
+	<p>por favor de dique unos minutos a leer cuidadosamente la informacion siguente suscrivirla con su firma al finalizar la lectura, muchas gracias</p>
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user" placeholder="Nombre:">
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user"  placeholder="Edad:">
+	<br>
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user" placeholder="Pais:">
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user" placeholder="E-mail:">
+	<br>
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user" placeholder="Hotel:">
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user" placeholder="Habitacion:">
+	<br>
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user" placeholder="Agencia de Viajes:">
+	<br>
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user" placeholder="Tratamiento:">
+	<input class="from-gruop has-success" style="border-radius:15px" type="text" name="user" placeholder="terapeuta:">
+
+	<p>1.- El mensaje o tratamiento que usted va a recibir tiene el proposito basico de la relagacion y el alivio de la tension muscular superficial</p>
+	
+</body>
+</html>
